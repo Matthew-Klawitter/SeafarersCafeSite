ea637946d38f57b2caabdcb011d8d8b31d0ce21c
Matthew Klawitter <mtklaw153@gmail.com>
2021-04-12 23:29:59 -0400

    First pass implementation of data binding

---
+++ src/components/home/studios/HomeStudioFeed.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="studio-feed">
+    <ul>
+      <li v-for="p in projects" :key="p">
+          {{ p }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+      return {
+        projects: [0, 1]
+      }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/home/HomeStudios.vue
+++ src/views/home/HomeStudios.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="home-studios">
     <p>Studios page</p>
+    <HomeStudioFeed/>
   </div>
 </template>
 
 <script>
+import HomeStudioFeed from '@/components/home/studios/HomeStudioFeed.vue';
+
 export default {
-  name: 'HomeStudios'
+  name: 'HomeStudios',
+  components: {
+    HomeStudioFeed
+  }
 }
 </script>
 
