917f7a09eb567dd6fc08e422c70c61d845f39120
Matthew Klawitter <mtklaw153@gmail.com>
2021-09-21 22:46:14 -0400

    Implemented basic blog frontend

---
+++ app/frontend/src/components/shared/blog/HomeBlogCard.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="blog-card">
+        <div class="blog-block-title">
+            <h2>{{post.title}}</h2>
+            <p><pre>  by {{post.author}} on {{post.createdAt}} (Edited: {{post.updatedAt}})</pre></p>
+            <p>"{{post.description}}"</p>
+            <hr>
+        </div>
+        <div class="blog-block-content">
+            <vue-markdown-plus>{{post.content}}</vue-markdown-plus>
+        </div>
+    </div>
+</template>
+
+<script>
+import VueMarkdownPlus from 'vue-markdown-plus';
+
+export default {
+    props: {
+        post: Object
+    },
+    components: {
+      VueMarkdownPlus
+    }
+}
+</script>
+
+<style scoped>
+  #blog-card {
+    width: 100%;
+    border: 1px solid rgb(40, 40, 40);
+    background-color: whitesmoke;
+  }
+  .blog-block-title {
+    width: 100%;
+    padding-top: 1rem;
+    padding-left: 1rem;
+  }
+  .blog-block-content {
+    width: 100%;
+    padding-top: .5rem;
+    padding-left: 1rem;
+  }
+</style>
--- app/frontend/src/views/home/HomeBlog.vue
+++ app/frontend/src/views/home/HomeBlog.vue
@@ -1,11 +1,51 @@
 <template>
-  <div id="home-blog-featured">
-    <p>Blog page</p>
+  <div id="home-blog">
+    <div class="blog-head">
+      <h1>The "Morning Mocha" Blog</h1>
+      <p>An aesthetic blog on our assorted caffinated thoughts</p>
+      <hr>
+    </div>
+    <p v-if="loading">Loading blog...</p>
+    <div class="blog-post-list" v-else>
+      <div v-for="p in posts" :key="p.id">
+        <HomeBlogCard :post="p"/>
+        <br>
+        <br>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import api from '@/api.js';
+import HomeBlogCard from '@/components/shared/blog/HomeBlogCard.vue';
+
+export default {
+  name: 'HomeBlog',
+  components: {
+    HomeBlogCard
+  },
+  data() {
+    return {
+      loading: false,
+      posts: null
+    }
+  },
+  async created() {
+    this.refreshProjects()
+  },
+  methods: {
+    async refreshProjects(){
+      this.loading = true;
+      this.posts = await api.getPosts();
+      this.loading = false;
+    }
+  }
+}
 </script>
 
 <style scoped>
+  .blog-head {
+      text-align: center;
+    }
 </style>
